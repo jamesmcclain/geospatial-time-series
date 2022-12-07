fb9303e61da6ceba8d48293ea60caf17b7d71976
James W. McClain <jmcclain@daystrom-data-concepts.com>
2022-12-07 15:56:38 -0500

    Rename directory

---
+++ src/scripts/train-embedder.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+python3 ./src/tokenizer/train.py --input-dir /data/ --output-dir ./ --epochs 42 --batch 32 --architecture cheaplab --train 1024 --eval 128
+python3 ./src/tokenizer/train.py --input-dir /data/ --output-dir ./ --epochs 42 --batch 8 --architecture resnet50
+python3 ./src/tokenizer/train.py --input-dir /data/ --output-dir ./ --epochs 42 --batch 12 --architecture resnet34
+python3 ./src/tokenizer/train.py --input-dir /data/ --output-dir ./ --epochs 42 --batch 16 --architecture resnet18
