fedf771c03656801f3ec8d8f2a9010a96d0d5ea0
James McClain <jmcclain@daystrom-data-concepts.com>
2023-01-26 16:32:14 -0500

    Segmentation (#9)
    * One attention weight per coefficient (instead of just one)
    
    * Segmentation over four classes, ignore "other" and "roads"
    
    * Positional encoding dimensions and W&B

--- src/transformer/train.py
+++ src/transformer/train.py
@@ -15,8 +15,18 @@ from PIL import Image
 
 from datasets import (InMemorySeasonalDataset, NpzSeriesDataset,
                       RawSeriesDataset)
-from models import (AttentionClassifier, BaselineClassifier, EntropyLoss,
+from models import (AttentionClassifier, AttentionSegmenter, BaselineClassifier, EntropyLoss,
                     ResnetTransformerClassifier)
+
+
+ARCHITECTURES = [
+    'attention-classifier',
+    'attention-segmenter',
+    'baseline-classifier',
+    'resnet-transformer-classifier',
+]
+DATASETS = ['in-memory-seasonal']
+RESNETS = ['resnet18', 'resnet34', 'resnet50']
 
 
 def worker_init_fn(i):
@@ -37,11 +47,10 @@ def cli_parser():
     parser = argparse.ArgumentParser()
 
     # Dataset, model type, input, output
-    parser.add_argument('--architecture', required=True, type=str, choices=['baseline-classifier', 'attention-classifier', 'resnet-transformer-classifier'])
-    parser.add_argument('--dataset', required=True, type=str, choices=['in-memory-seasonal'])
-    parser.add_argument('--input-dir', required=False, type=str)
+    parser.add_argument('--architecture', required=True, type=str, choices=ARCHITECTURES)
+    parser.add_argument('--dataset', required=True, type=str, choices=DATASETS)
     parser.add_argument('--output-dir', required=False, type=str)
-    parser.add_argument('--resnet-architecture', required=False, type=str, choices=['resnet18', 'resnet34', 'resnet50'])
+    parser.add_argument('--resnet-architecture', required=False, type=str, choices=RESNETS)
     parser.add_argument('--resnet-state', required=False, type=str)
     parser.add_argument('--series', required=False, type=str, nargs='+')
     parser.add_argument('--size', required=False, type=int, default=256)
@@ -50,7 +59,7 @@ def cli_parser():
     # Hyperparameters
     parser.add_argument('--eval-batches', required=False, type=int)
     parser.add_argument('--train-batches', required=False, type=int)
-    parser.add_argument('--batch-size', required=False, type=int, default=16)
+    parser.add_argument('--batch-size', required=False, type=int, default=4)
 
     parser.add_argument('--dimensions', required=False, type=int, default=512)
     parser.add_argument('--encoder-layers', required=False, type=int, default=1)
@@ -59,7 +68,9 @@ def cli_parser():
     # parser.add_argument('--entropy', dest='entropy', default=False, action='store_true')
     parser.add_argument('--epochs', required=False, type=int, default=2**7)
     parser.add_argument('--gamma', required=False, type=float, default=0.7)
-    parser.add_argument('--lr', required=False, type=float, default=3e-4)
+
+    parser.add_argument('--lr', required=False, type=float, default=1e-3)
+    parser.add_argument('--clip', required=False, type=float, default=1)
 
     parser.add_argument('--sequence-limit', required=False, type=int, default=10)
     parser.add_argument('--train-ipi', required=False, type=int, default=None)
@@ -88,7 +99,11 @@ if __name__ == '__main__':
 
     try:
         import wandb
-        wandb.init(project="geospatial-time-series",
+        if 'classifier' in args.architecture:
+            project = 'geospatial-time-series classification'
+        elif 'segmenter' in args.architecture:
+            project = 'geospatial-time-series segmentation'
+        wandb.init(project=project,
                    config={
                        "learning_rate": args.lr,
                        "training_batches": args.train_batches,
@@ -136,9 +151,9 @@ if __name__ == '__main__':
                                     args.target,
                                     iters_per_incr=train_ipi,
                                     size=args.size,
-                                    dimensions=512,
+                                    dimensions=args.dimensions,
                                     sequence_limit=args.sequence_limit,
-                                    digest_labels=True,
+                                    digest_labels='classifier' in args.architecture,
                                     evaluation=False),
             **dataloader_cfg,
         )
@@ -147,21 +162,25 @@ if __name__ == '__main__':
                                     args.target,
                                     iters_per_incr=eval_ipi,
                                     size=args.size,
-                                    dimensions=512,
+                                    dimensions=args.dimensions,
                                     sequence_limit=args.sequence_limit,
-                                    digest_labels=True,
+                                    digest_labels='classifier' in args.architecture,
                                     evaluation=True),
             **dataloader_cfg,
         )
 
-    if args.dataset == 'in-memory-seasonal':
         train_dl = iter(train_dl)
         eval_dl = iter(eval_dl)
 
     # ------------------------------------------------------------------------
 
-    _, clss = next(train_dl)[1].shape
     device = torch.device('cuda')
+
+    if 'classifier' in args.architecture:
+        _, clss = next(train_dl)[1].shape
+    elif 'segmenter' in args.architecture:
+        clss = 4  # XXX
+
     if args.architecture == 'series-resnet-classifier':
         assert args.resnet_architecture is not None
         assert args.resnet_state is not None
@@ -172,21 +191,9 @@ if __name__ == '__main__':
             args.resnet_architecture,
             args.resnet_state,
             args.size,
-            # args.dimensions + 512,
-            args.dimensions + 0,
+            args.dimensions,
             args.num_heads,
             args.encoder_layers,
-            clss=clss,
-        ).to(device)
-    elif args.architecture == 'attention-classifier':
-        assert args.resnet_architecture is not None
-        assert args.resnet_state is not None
-        assert args.dimensions is not None
-        model = AttentionClassifier(
-            args.resnet_architecture,
-            args.resnet_state,
-            args.size,
-            args.dimensions,
             clss=clss,
         ).to(device)
     elif args.architecture == 'baseline-classifier':
@@ -200,8 +207,37 @@ if __name__ == '__main__':
             args.dimensions,
             clss=clss,
         ).to(device)
+    elif args.architecture == 'attention-classifier':
+        assert args.resnet_architecture is not None
+        assert args.resnet_state is not None
+        assert args.dimensions is not None
+        model = AttentionClassifier(
+            args.resnet_architecture,
+            args.resnet_state,
+            args.size,
+            args.dimensions,
+            clss=clss,
+        ).to(device)
+    elif args.architecture == 'attention-segmenter':
+        assert args.resnet_architecture is not None
+        assert args.resnet_state is not None
+        assert args.dimensions is not None
+        model = AttentionSegmenter(
+            args.resnet_architecture,
+            args.resnet_state,
+            args.size,
+            args.dimensions,
+            clss=clss,
+        ).to(device)
 
-    obj1 = torch.nn.MSELoss().to(device)
+    if 'classifier' in args.architecture:
+        obj = torch.nn.MSELoss().to(device)
+    elif 'segmenter' in args.architecture:
+        obj = torch.nn.CrossEntropyLoss(
+            weight=torch.Tensor([1e-6, 1., 1., 1e-6]),
+            ignore_index=-1).to(device)
+    else:
+        raise Exception()
     opt = torch.optim.AdamW(model.parameters(), lr=args.lr)
     sched = torch.optim.lr_scheduler.StepLR(opt, step_size=1, gamma=args.gamma)
 
@@ -218,21 +254,23 @@ if __name__ == '__main__':
                 batches = args.train_batches
                 for _ in tqdm.tqdm(range(0, batches),
                                    desc=f'Epoch {epoch}: training'):
+                    opt.zero_grad()
+
                     batch = next(train_dl)
                     x = batch[0].to(device)
                     pos = batch[2].to(device)
                     target = batch[1].to(device)
-                    # yapf: disable
-                    if args.architecture in {'attention-classifier', 'resnet-transformer-classifier'}:
+                    # if 'segmenter' in args.architecture:
+                    #     target = target-1  # XXX
+                    if args.architecture in {'attention-segmenter', 'attention-classifier', 'resnet-transformer-classifier'}:
                         out = model(x, pos)
                     elif args.architecture in {'baseline-classifier'}:
                         out = model(x)
-                    # yapf: enable
-                    loss = obj1(out, target)
+                    loss = obj(out, target)
                     loss_float.append(loss.item())
 
-                    opt.zero_grad()
                     loss.backward()
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
                     opt.step()
             elif mode == 'eval':
                 model.eval()
@@ -246,27 +284,29 @@ if __name__ == '__main__':
                         x = batch[0].to(device)
                         pos = batch[2].to(device)
                         target = batch[1].to(device)
-                        # yapf: disable
+                        # if 'segmenter' in args.architecture:
+                        #     target = target-1  # XXX
                         if args.architecture in {'attention-classifier', 'resnet-transformer-classifier'}:
                             out = model(x, pos)
                         elif args.architecture in {'baseline-classifier'}:
                             out = model(x)
-                        # yapf: enable
-                        gt = batch[1].detach().cpu().numpy()
-                        pred = out.detach().cpu().numpy()
-                        gts.append(gt)
-                        preds.append(pred)
-                        loss = obj1(out, target)
+                        if 'classifier' in args.architecture:
+                            gt = batch[1].detach().cpu().numpy()
+                            pred = out.detach().cpu().numpy()
+                            gts.append(gt)
+                            preds.append(pred)
+                        loss = obj(out, target)
                         loss_float.append(loss.item())
 
-                gts = np.concatenate(gts, axis=0)
-                preds = np.concatenate(preds, axis=0)
-                diffs = (gts - preds)
-                mus = np.mean(diffs, axis=0)
-                absmus = np.mean(np.absolute(diffs), axis=0)
-                sigmas = np.sqrt(np.mean(np.power(diffs, 2), axis=0))
-                gts = np.mean(gts, axis=0)
-                preds = np.mean(preds, axis=0)
+                if 'classifier' in args.architecture:
+                    gts = np.concatenate(gts, axis=0)
+                    preds = np.concatenate(preds, axis=0)
+                    diffs = (gts - preds)
+                    mus = np.mean(diffs, axis=0)
+                    absmus = np.mean(np.absolute(diffs), axis=0)
+                    sigmas = np.sqrt(np.mean(np.power(diffs, 2), axis=0))
+                    gts = np.mean(gts, axis=0)
+                    preds = np.mean(preds, axis=0)
 
             loss_float = np.mean(loss_float)
 
@@ -278,27 +318,34 @@ if __name__ == '__main__':
         # yapf: disable
         if loss_e < best:
             best = loss_e
-            log.info(
-                f'✓ Epoch={epoch} train={loss_t} eval={loss_e} '
-                f'gt={gts} pred={preds} μ={mus} σ={sigmas}'
-            )
+            if 'classifier' in args.architecture:
+                log.info(
+                    f'✓ Epoch={epoch} train={loss_t} eval={loss_e} '
+                    f'gt={gts} pred={preds} μ={mus} σ={sigmas}'
+                )
+            else:
+                log.info(f'✓ Epoch={epoch} train={loss_t} eval={loss_e}')
             if args.output_dir:
                 torch.save(model.state_dict(), f'{args.output_dir}/{args.architecture}-{args.resnet_architecture}-best.pth')
         else:
-            log.info(
-                f'✗ Epoch={epoch} train={loss_t} eval={loss_e} '
-                f'gt={gts} pred={preds} μ={mus} σ={sigmas}'
-            )
+            if 'classifier' in args.architecture:
+                log.info(
+                    f'✗ Epoch={epoch} train={loss_t} eval={loss_e} '
+                    f'gt={gts} pred={preds} μ={mus} σ={sigmas}'
+                )
+            else:
+                log.info(f'✗ Epoch={epoch} train={loss_t} eval={loss_e}')
         # yapf: enable
         try:
             wandb_dict = {
                 "loss train": loss_t,
                 "loss eval": loss_e,
             }
-            for i in range(len(mus)):
-                wandb_dict.update({f'μ{i}': mus[i]})
-                wandb_dict.update({f'abs μ{i}': absmus[i]})
-                wandb_dict.update({f'σ{i}': sigmas[i]})
+            if 'classifier' in args.architecture:
+                for i in range(len(mus)):
+                    wandb_dict.update({f'μ{i}': mus[i]})
+                    wandb_dict.update({f'abs μ{i}': absmus[i]})
+                    wandb_dict.update({f'σ{i}': sigmas[i]})
             wandb.log(wandb_dict)
         except:
             pass
