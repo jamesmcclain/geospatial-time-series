1f8c4202cfc4c78477e2264f87c7446c019b69d9
Adeel Hassan <ahassan@element84.com>
2023-06-22 12:55:08 -0400

    add hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+from src.attention.models import (AttentionSegmenter, CheaplabLiteSegmenter,
+                                  CheaplabSegmenter, EntropyLoss)
+from src.unsupervised_pretrain.models import (
+    SeriesModel, SeriesResNet18, SeriesEfficientNetb0, SeriesMobileNetv3)
