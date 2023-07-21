1a0c372c13bab04871ab0d162650aff60c3f9e54
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-07-21 15:38:18 -0400

    Embedding script

---
+++ src/unsupervised_pretrain/embed_text_labels.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2023
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import glob
+import json
+import logging
+import sys
+
+import numpy as np
+import torch
+import tqdm
+from InstructorEmbedding import INSTRUCTOR
+
+if __name__ == "__main__":
+    # yapf: disable
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_file", type=str, help="Where to read the labels from")
+    parser.add_argument("npy_file", type=str, help="Where to write the embeddings to")
+    parser.add_argument("--instruction", type=str, default=None, help="The instruction to provide to instructor-xl")
+    args = parser.parse_args()
+    # yapf: enable
+
+    # yapf: disable
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="%(asctime)-15s %(message)s")
+    log = logging.getLogger()
+    # yapf: enable
+
+    if args.instruction is None:
+        instruction = "Represent the geospatial data (the number, proportion, and description of geographic features) for clustering:"
+    else:
+        instruction = args.instruction
+
+    with open(args.json_file, "r") as f:
+        labels = json.load(f)
+
+    model = INSTRUCTOR("hkunlp/instructor-xl").to("cuda")
+    model.max_seq_length = 4096
+
+    log.info("Producing embeddings")
+    submissions = []
+    for label in tqdm.tqdm(labels):
+        submission = [instruction, label]
+        submissions.append(submission)
+
+    embeddings = model.encode(submissions)
+
+    log.info("Saving")
+    np.save(args.npy_file, embeddings)
