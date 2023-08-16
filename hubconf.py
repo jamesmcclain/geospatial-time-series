c9cc35ebd7044b0500c7fbe407f70889d06308c6
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-08-16 12:09:21 -0400

    Delete

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,5 @@
 # flake8: noqa
 
-from src.attention.models import (AttentionSegmenter, CheaplabLiteSegmenter,
-                                  CheaplabSegmenter, EntropyLoss)
 from src.unsupervised_pretrain.models import (Hat, SeriesEfficientNetb0,
                                               SeriesMobileNetv3, SeriesModel,
                                               SeriesResNet18)
