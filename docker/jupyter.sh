8eb2a8b82a8b8703b218275bce6a3c0628b1f803
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-09-07 17:55:26 -0400

    Rearrange docker stuff

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8888:8888 --shm-size ${2:-16}G --runtime=nvidia -it --rm -v $(pwd):/workdir -v ${1:-/tmp}:/datasets:ro -w /workdir timeseries:2.0.1-cuda11.7-cudnn8-jupyter
+docker run -p 8888:8888 --shm-size ${2:-16}G --runtime=nvidia -it --rm -v $(pwd):/workdir -v ${1:-/tmp}:/datasets:ro -w /workdir timeseries-jupyter
