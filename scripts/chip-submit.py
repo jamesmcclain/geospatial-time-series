9d3bafc478c5d4fbb997e86c32aa378916ddf74a
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-09-11 22:42:23 -0400

    Impose time limit on jobs

--- scripts/chip-submit.py
+++ scripts/chip-submit.py
@@ -2,12 +2,12 @@
 
 import argparse
 import logging
+
 import boto3
 from sagemaker.processing import ScriptProcessor, ProcessingInput, ProcessingOutput
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.workflow.pipeline_context import PipelineSession
 from sagemaker.workflow.steps import ProcessingStep, TrainingStep
-
 
 if __name__ == "__main__":
 
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--instance-count", type=int, required=True, help="The number of instances to use")
     parser.add_argument("--instance-type", type=str, required=False, default="ml.t3.medium", help="The instance type")
     parser.add_argument("--output", type=str, required=True, help="The location on S3 where outputs will be deposited")
+    parser.add_argument("--minutes-max", type=int, required=False, default=20, help="The maximum number of minutes to allow the job")
     # chip-related
     parser.add_argument("--series", type=int, required=True, help="The number of chips to collect")
     parser.add_argument("--good-data-threshold", type=float, required=False, default=0.80, help="The minimum proportion of pixels needed for a chip to be \"good\"")
@@ -55,6 +56,7 @@ if __name__ == "__main__":
         instance_count=args.instance_count,
         instance_type=args.instance_type,
         sagemaker_session=sagemaker_session,
+        max_runtime_in_seconds=args.minutes_max * 60,
         command=["python3"],
     )
     step_args = step_processor.run(
