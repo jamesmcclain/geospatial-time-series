28ed8dcfc25d871ea8271bd014e3ae7d1f79e098
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-09-20 10:01:32 -0400

    Formatting

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,10 @@
 # flake8: noqa
 
 from pretrain.autoencoders import MultiViewAutoencoder
-from pretrain.models import (SeriesEfficientNetb0,
-                             SeriesMobileNetv3,
-                             SeriesResNet18, SeriesResNet34,
-                             SeriesResNet50)
+from pretrain.models import (
+    SeriesEfficientNetb0,
+    SeriesMobileNetv3,
+    SeriesResNet18,
+    SeriesResNet34,
+    SeriesResNet50,
+)
