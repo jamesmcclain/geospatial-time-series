ff790ee37ed3e6b22aa9b878a19aea32299675b4
James W. McClain <jmcclain@daystrom-data-concepts.com>
2023-09-22 17:53:08 -0400

    Add category-finding and querying script

---
+++ scripts/farmfind.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python3
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2023
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import glob
+import json
+import logging
+
+import numpy as np
+import pandas as pd
+import tqdm
+
+
+def is_farmland(tags_dict):
+    farmland = ("landuse", "farmland") in tags_dict.items()
+    crop = "crop" in tags_dict.keys()
+    return farmland and crop
+
+
+def is_orchard(tags_dict):
+    return ("landuse", "orchard") in tags_dict.items()
+
+
+def is_meadow(tags_dict):
+    return ("landuse", "meadow") in tags_dict.items()
+
+
+def is_vineyard(tags_dict):
+    return ("landuse", "vineyard") in tags_dict.items()
+
+
+def is_residential(tags_dict):
+    return ("landuse", "residential") in tags_dict.items()
+
+
+def is_industrial(tags_dict):
+    return ("landuse", "industrial") in tags_dict.items()
+
+
+def is_grass(tags_dict):
+    return ("landuse", "grass") in tags_dict.items()
+
+
+def farmland_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score += 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def orchard_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score += 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def meadow_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score += 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def vineyard_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score += 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def residential_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score += 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def industrial_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score += 1
+    if is_grass(tags_dict):
+        score -= 1
+    return score
+
+
+def grass_score(tags_dict):
+    if tags_dict is None:
+        return 0
+    score = 0
+    if is_farmland(tags_dict):
+        score -= 1
+    if is_orchard(tags_dict):
+        score -= 1
+    if is_meadow(tags_dict):
+        score -= 1
+    if is_vineyard(tags_dict):
+        score -= 1
+    if is_residential(tags_dict):
+        score -= 1
+    if is_industrial(tags_dict):
+        score -= 1
+    if is_grass(tags_dict):
+        score += 1
+    return score
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+
+    # yapf: disable
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--daylight-relations", type=str, required=True, help="Directory containing Daylight Map Distribution relations parquet files")
+    parser.add_argument("--k", type=int, required=False, default=3, help="How many top k quadkeys to fetch")
+    parser.add_argument("--quadkey-json", required=False, default="quadkey.json", help="Where to store the discovered quadkeys")
+    args = parser.parse_args()
+    # yapf: enable
+
+    log.info(
+        f"loading relations from Daylight Map Distribution from {args.daylight_relations}"
+    )
+    parquet_files = glob.glob(f"{args.daylight_relations}/*", recursive=True)
+    df = pd.read_parquet(parquet_files)
+
+    df["quadkey_11"] = df["quadkey"].str[:-4]
+
+    key_fn_pairs = [
+        ("farmland_score", farmland_score),
+        ("orchard_score", orchard_score),
+        ("meadow_score", meadow_score),
+        ("vineyard_score", vineyard_score),
+        ("residential_score", residential_score),
+        ("industrial_score", industrial_score),
+        ("grass_score", grass_score),
+    ]
+
+    output = {}
+
+    for key, fn in tqdm.tqdm(key_fn_pairs):
+        df[key] = df["tags"].apply(fn)
+        output.update({key: []})
+        for quadkey, score in (
+            df.groupby("quadkey_11")[key]
+            .sum()
+            .sort_values(ascending=False)
+            .head(args.k)
+            .items()
+        ):
+            output.get(key).append([quadkey, score])
+            log.info(f"quadkey_11={quadkey_11} score={score}")
+        df.drop(columns=[key], inplace=True)
+
+        with open(args.quadkey_json, "w") as json_file:
+            json.dump(output, json_file, indent=4)
---
+++ scripts/quadkey-to-geojson.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+# BSD 3-Clause License
+#
+# Copyright (c) 2023
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import json
+from typing import List, Tuple
+
+from pyquadkey2 import quadkey
+
+
+def quadkey_to_geojson_polygon(qk: str) -> dict:
+    # Get the tile from the quadkey
+    tile = quadkey.from_str(qk)
+
+    # Get the bounding box of the tile
+    bbox_nw = tile.to_geo(quadkey.TileAnchor.ANCHOR_NW)
+    bbox_se = tile.to_geo(quadkey.TileAnchor.ANCHOR_SE)
+
+    geojson_polygon = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [bbox_nw[1], bbox_nw[0]],
+                    [bbox_se[1], bbox_nw[0]],
+                    [bbox_se[1], bbox_se[0]],
+                    [bbox_nw[1], bbox_se[0]],
+                    [bbox_nw[1], bbox_nw[0]],
+                ]
+            ],
+        },
+        "properties": {"quadkey": qk},
+    }
+
+    return geojson_polygon
+
+
+def main():
+    # yapf: disable
+    parser = argparse.ArgumentParser(description="Convert Bing Map Tile quadkeys to GeoJSON bounding boxes.")
+    parser.add_argument("--input-file", help="Path to the input JSON file containing quadkeys.")
+    parser.add_argument("--output-file", help="Path to the output GeoJSON file.")
+    parser.add_argument("--key", required=True, help="The key within the json file to use.")
+    args = parser.parse_args()
+    # yapf: enable
+
+    with open(args.input_file, "r") as f:
+        data = json.load(f)
+
+    quadkeys = [item[0] for item in data[args.key]]
+
+    features = [quadkey_to_geojson_polygon(qk) for qk in quadkeys]
+
+    geojson_collection = {"type": "FeatureCollection", "features": features}
+
+    with open(args.output_file, "w") as f:
+        return json.dump(geojson_collection, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
